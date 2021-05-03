26b900e7499f8f53e04febc08badbb02ca6ffadd
Bryan Long <bryan.m.long@accenture.com>
2021-05-03 19:31:52 -0400

    svsdvsd

--- rhel82hana.sh
+++ rhel82hana.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-mkdir /tmp/rpm && cd /tmp/rpm
+mkdir /tmp/rpm
+cd /tmp/rpm
 wget https://github.com/bryalong77/code/raw/main/hana/compat-sap-c%2B%2B-10-10.2.1-1.el8_2.x86_64.rpm
 yum localinstall /tmp/rpm/compat-sap-c%2B%2B-10-10.2.1-1.el8_2.x86_64.rpm
 https://github.com/bryalong77/code/raw/main/hana/compat-sap-c%2B%2B-9-9.1.1-2.2.el8.x86_64.rpm
