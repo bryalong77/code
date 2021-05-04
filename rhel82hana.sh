f1d5dd773a166acb90c0fea62bad39a204951186
Bryan Long <bryan.m.long@accenture.com>
2021-05-04 00:22:28 -0400

    dddb

--- rhel82hana.sh
+++ rhel82hana.sh
@@ -6,17 +6,3 @@ cat >> /etc/motd << EOF
 **           Image: RHEL 8.2 for HANA                **
 *******************************************************
 EOF
-mkdir /tmp/rpm
-cd /tmp/rpm
-wget https://github.com/bryalong77/code/raw/main/hana/compat-sap-c%2B%2B-10-10.2.1-1.el8_2.x86_64.rpm
-yum localinstall /tmp/rpm/compat-sap-c%2B%2B-10-10.2.1-1.el8_2.x86_64.rpm
-wget https://github.com/bryalong77/code/raw/main/hana/compat-sap-c%2B%2B-9-9.1.1-2.2.el8.x86_64.rpm
-yum localinstall /tmp/rpm/compat-sap-c%2B%2B-9-9.1.1-2.2.el8.x86_64.rpm
-wget https://github.com/bryalong77/code/raw/main/hana/resource-agents-sap-hana-0.154.0-2.el8.noarch.rpm
-yum localinstall /tmp/rpm/resource-agents-sap-hana-0.154.0-2.el8.noarch.rpm
-wget https://github.com/bryalong77/code/raw/main/hana/resource-agents-sap-hana-scaleout-0.164.0-1.el8.noarch.rpm
-yum localinstall /tmp/rpm/resource-agents-sap-hana-scaleout-0.164.0-1.el8.noarch.rpm
-wget https://github.com/bryalong77/code/raw/main/hana/rhel-system-roles-sap-2.0.0-7.el8.noarch.rpm
-yum localinstall /tmp/rpm/rhel-system-roles-sap-2.0.0-7.el8.noarch.rpm
-wget https://github.com/bryalong77/code/raw/main/hana/tuned-profiles-sap-hana-2.14.0-3.el8_3.2.noarch.rpm
-yum localinstall /tmp/rpm/tuned-profiles-sap-hana-2.14.0-3.el8_3.2.noarch.rpm
