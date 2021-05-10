548c38bf760a5027f702ea7a8b17e4a66c486f13
Bryan Long <bryan.m.long@accenture.com>
2021-05-10 11:42:14 -0400

    bdfbfdbd

--- rhel77sap.sh
+++ rhel77sap.sh
@@ -4,5 +4,14 @@ cat >> /etc/motd << EOF
 **           This VM was built from the:             **
 **          !! AZURE VM IMAGE BUILDER !!             **
 **           Image: RHEL 7.7 for SAP                 **
+**          version: 2 date: 5/10/2021               **
 *******************************************************
 EOF
+subscription-manager register --username=bryan.m.long@avanade.com --password=Benismyhero1
+yum -y install nfs-utils
+yum -y update
+subscription-manager clean
+lvextend -L 10G /dev/rootvg/tmplv
+lvextend -l 100%FREE /dev/rootvg/rootlv
+xfs_growfs /dev/rootvg/tmplv
+xfs_growfs /dev/rootvg/rootlv
