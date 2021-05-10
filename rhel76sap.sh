3cd0852f173257d925e7aab9f86cb9a1edd7de08
Bryan Long <bryan.m.long@accenture.com>
2021-05-10 13:13:52 -0400

    wefewfewf

--- rhel76sap.sh
+++ rhel76sap.sh
@@ -7,12 +7,3 @@ cat >> /etc/motd << EOF
 **          version: 2 date: 5/10/2021               **
 *******************************************************
 EOF
-subscription-manager register --username=bryan.m.long@avanade.com --password=Benismyhero1
-subscription-manager attach --pool 8a0080897796d9d10177b5eff1a407d0
-yum -y install nfs-utils
-yum -y update
-subscription-manager clean
-lvextend -L 10G /dev/rootvg/tmplv
-lvextend -l 100%FREE /dev/rootvg/rootlv
-xfs_growfs /dev/rootvg/tmplv
-xfs_growfs /dev/rootvg/rootlv
