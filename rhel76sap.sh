828a40ede108931f5b61d8fd001daa0dcf22fa79
Bryan Long <bryan.m.long@accenture.com>
2021-05-10 11:02:22 -0400

    derger

--- rhel76sap.sh
+++ rhel76sap.sh
@@ -4,6 +4,7 @@ cat >> /etc/motd << EOF
 **           This VM was built from the:             **
 **          !! AZURE VM IMAGE BUILDER !!             **
 **           Image: RHEL 7.6 for SAP                 **
+**          version: 2 date: 5/10/2021               **
 *******************************************************
 EOF
 subscription-manager register --username=bryan.m.long@avanade.com --password=Benismyhero1
